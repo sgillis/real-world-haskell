59bc32fd164bdd67d5682ac2b3393a7859b1fb68
San Gillis <gillis.san@gmail.com>
2014-03-30 00:13:34 +0100

    splitWith function

--- chapter04/chapter04.hs
+++ chapter04/chapter04.hs
@@ -12,3 +12,9 @@ safeLast xs = safeHead $ reverse xs
 safeInit :: [a] -> Maybe [a]
 safeInit [] = Nothing
 safeInit xs = Just $ init xs
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith f xs
+    | null snd  = [fst]
+    | otherwise = fst : splitWith f (tail snd)
+    where (fst, snd) = break f xs
