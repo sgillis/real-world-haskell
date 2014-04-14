c112fd5a5967b348d9104c7875b8c141566fa3d5
San Gillis <gillis.san@gmail.com>
2014-04-14 22:06:22 +0200

    Finish parsebyte

--- chapter10/Parse.hs
+++ chapter10/Parse.hs
@@ -24,6 +24,25 @@ parseByte =
           where newState = initState { string = remainder, offset = newOffset }
                 newOffset = offset initState + 1
 
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+            "byte offset " ++ show (offset s) ++ ": " ++ err
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+  where chainedParser initState =
+          case runParse firstParser initState of
+            Left errMessage ->
+                Left errMessage
+            Right (firstResult, newState) ->
+                runParse (secondParser firstResult) newState
+
 parse :: Parse a -> L.ByteString -> Either String a
 parse parser initState =
     case runParse parser (ParseState initState 0) of
