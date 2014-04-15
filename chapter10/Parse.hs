d97b36e7eba094b72412324e450477f0753a4b2a
San Gillis <gillis.san@gmail.com>
2014-04-15 22:11:20 +0200

    Finished Parse library

--- chapter10/Parse.hs
+++ chapter10/Parse.hs
@@ -1,6 +1,22 @@
+module Parse ( identity
+             , parseWhileWith
+             , parseByte
+             , bail
+             , (==>)
+             , (==>&)
+             , assert
+             , w2c
+             , parseBytes
+             , parseNat
+             , skipSpaces
+             ) where
+
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Int (Int64)
 import Data.Word (Word8)
+import Data.Char (chr, isSpace, isDigit)
+import Data.Functor ((<$>))
 
 data ParseState = ParseState {
       string :: L.ByteString
@@ -10,6 +26,10 @@ data ParseState = ParseState {
 newtype Parse a = Parse {
     runParse :: ParseState -> Either String (a, ParseState)
 }
+
+instance Functor Parse where
+    fmap f parser = parser ==> \result ->
+                    identity (f result)
 
 identity :: a -> Parse a
 identity a = Parse (\s -> Right (a, s))
@@ -43,8 +63,64 @@ firstParser ==> secondParser = Parse chainedParser
             Right (firstResult, newState) ->
                 runParse (secondParser firstResult) newState
 
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> \_ -> f
+
+assert :: Bool -> String -> Parse ()
+assert True  _   = identity ()
+assert False err = bail err
+
 parse :: Parse a -> L.ByteString -> Either String a
 parse parser initState =
     case runParse parser (ParseState initState 0) of
         Left err          -> Left err
         Right (result, _) -> Right result
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
+
+peekChar :: Parse (Maybe Char)
+peekChar = fmap w2c <$> peekByte
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte) ==> \mp ->
+               if mp == Just True
+               then parseByte ==> \b ->
+                   (b:) <$> parseWhile p
+               else identity []
+
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
+
+parseBytes :: Int -> Parse L.ByteString
+parseBytes n =
+    getState ==> \st ->
+    let n' = fromIntegral n
+        (h, t) = L.splitAt n' (string st)
+        st' = st { offset = offset st + L.length h, string = t }
+    in putState st' ==>&
+       assert (L.length h == n') "end of input" ==>&
+       identity h
+
+parseNat :: Parse Int
+parseNat = parseWhileWith w2c isDigit ==> \digits ->
+           if null digits
+           then bail "no more input"
+           else let n = read digits
+                in if n < 0
+                   then bail "integer overflow"
+                   else identity n
+
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+
+-- main = do
+--     let x = L8.pack "foo"
+--     return (parse (parseByte ==> \a -> parseByte ==> \b -> parseByte ==> \c -> identity (a,b,c)) x)
+--     return (parse (peekByte ==> \firstbyte -> getState ==> \state -> identity (firstbyte, state)) x)
